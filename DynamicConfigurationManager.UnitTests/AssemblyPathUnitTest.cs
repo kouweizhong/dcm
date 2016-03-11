1c18ce0f3570c8665752e7fd5f478475ef5bea93
dstrommer <dstrommer@hotmail.com>
2016-03-10 20:39:42 -0500

    Adding unit tests

---
+++ DynamicConfigurationManager.UnitTests/AssemblyPathUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DynamicConfigurationManager.UnitTests
+{
+    [TestClass]
+    public class AssemblyPathUnitTest
+    {
+        [TestMethod]
+        public void AssemblyPath_ShouldPass_If_FileNotFound()
+        {
+            string rtn = DynamicConfigurationService.AppSettings["assemblyPathKey1"];
+            Assert.IsNull(rtn);
+        }
+
+        [TestMethod]
+        public void AssemblyPath_ShouldPass_If_FileFound()
+        {
+            string rtn = DynamicConfigurationService.AppSettings["assemblyPathKey2"];
+            Assert.AreEqual("Success", rtn);
+        }
+
+        [TestMethod]
+        public void AssemblyPathRegEx_ShouldPass_If_FileFound()
+        {
+            string rtn = DynamicConfigurationService.AppSettings["assemblyPathRegExKey"];
+            Assert.AreEqual("Success", rtn);
+        }
+
+        [TestMethod]
+        public void AssemblyPathRegEx_ShouldPass_If_FileNotFound()
+        {
+            string rtn = DynamicConfigurationService.AppSettings["assemblyPathRegExBad"];
+            Assert.IsNull(rtn);
+        }
+    }
+}
--- DynamicConfigurationManager.UnitTests/GlobalAppSettingUnitTest.cs
+++ DynamicConfigurationManager.UnitTests/GlobalAppSettingUnitTest.cs
@@ -1,19 +1,19 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace DynamicConfigurationManager.Tests
+namespace DynamicConfigurationManager.UnitTests
 {
     [TestClass]
     public class GlobalAppSettingUnitTest
     {
         [TestMethod]
-        public void GlobalAppSettingKey1()
+        public void GlobalAppSetting_ShouldPass_If_Key1Found()
         {
             string rtn = DynamicConfigurationService.AppSettings["globalKey1"];
             Assert.AreEqual("globalValue1", rtn);
         }
 
         [TestMethod]
-        public void GlobalAppSettingKey2()
+        public void GlobalAppSetting_ShouldPass_If_Key2Found()
         {
             string rtn = DynamicConfigurationService.AppSettings["globalKey2"];
             Assert.AreEqual("globalValue2", rtn);
